39ca527fb0d21c302fe82ec8d189b7fed2daeb1b
dldmsql <yhy5913@gmail.com>
2023-05-15 01:35:47 +0900

    style: header, footer 컴포넌트화 및 적용

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="app">
+    <base-header></base-header>
+    <base-footer></base-footer>
+  </div>
 </template>
+<script lang="ts">
+import Vue from 'vue'
+import BaseHeader from '@/components/Header.vue'
+import BaseFooter from './components/Footer.vue';
+export default {
+  components : {
+    BaseHeader,
+    BaseFooter
+  }
+}
+</script>
 
 <style lang="stylus">
 #app
---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+    <header>
+        <nav>
+            <router-link to="/">Home</router-link> |
+            <router-link to="/about">About</router-link>
+        </nav>
+        <router-view />
+    </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { Vue } from 'vue-class-component'
+
+export default class BaseHeader extends Vue{
+
+}
+</script>
+
+<style scoped></style>
