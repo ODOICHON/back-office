267f616ce63a9156335f4587291bedafce8a781a
dldmsql <yhy5913@gmail.com>
2023-05-15 02:28:51 +0900

    feat: 라우팅 추가

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,11 +8,23 @@ const routes: Array<RouteRecordRaw> = [
     component: HomeView
   },
   {
+    path: '/intro',
+    name: 'intro',
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/skill',
+    name: 'skill',
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/review',
+    name: 'review',
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   }
 ]
