6a8ae4ffd393c8a92c55ae3ec5c58b2072684e6d
dldmsql <yhy5913@gmail.com>
2023-05-15 02:29:18 +0900

    style: 게시글 리스트와 사이드바 모듈화 적용

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,36 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="row g-5">
+    <board-list></board-list>
+    <side-var></side-var>
+    <!-- TO-DO 페이지네이션으로 페이지 수 받아오면 v-for로 리펙토링 -->
+    <div class="page-conatiner">
+        <a href="#" class="stretched-link">1</a>  
+        <sapn>  2   </sapn>
+        <sapn>  3   </sapn>
+        <sapn>  4   </sapn>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+import BoardList from '@/components/BoardList.vue'
+import SideVar from '@/components/SideVar.vue'
 
 @Options({
   components: {
-    HelloWorld
+    BoardList,
+    SideVar
   }
 })
-export default class HomeView extends Vue {}
+export default class HomeView extends Vue {
+
+}
 </script>
+
+<style scoped>
+.page-conatiner {
+    margin-bottom: 20px;
+}
+
+</style>
