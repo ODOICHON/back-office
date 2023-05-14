66353fd4c352ffc4a1c4848165175f00d0ba2e2d
dldmsql <yhy5913@gmail.com>
2023-05-15 00:39:24 +0900

    chore: proxy 등록

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,13 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
+module.exports = {
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'https://duaily.net',
+        changeOrigin: true
+      }
+    }
+  },
+  lintOnSave: false,
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production' ? '/tech/' : '/'
-})
+}
