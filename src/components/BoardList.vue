e7f996f399d11d55dc4132e70300af70e8e55389
dldmsql <yhy5913@gmail.com>
2023-05-18 01:09:31 +0900

    style: Bootstrap CSS 이슈 해결

--- src/components/BoardList.vue
+++ src/components/BoardList.vue
@@ -1,48 +1,14 @@
 <template>
-    <div class="col mb-2" id="board-container">
+    <div class="col mb-2" id="board-container" >
         <!-- 게시글 카드 4개씩 페이지네이션  -->
-        <div class="col-md-10 text-start">
+        <div class="row-md-5 text-start" v-for="board in boards" :key="board.boardId">
             <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
                 <div class="col p-4 d-flex flex-column position-static">
-                    <strong class="d-inline-block mb-2 text-primary">이거는 태그?</strong>
-                    <h3 class="mb-0">게시물 제목!</h3>
-                    <div class="mb-1 text-body-secondary">게시글 작성 일자 ( 05.11.2023 )</div>
-                    <p class="card-text mb-auto">게시글 내용 미리보기 문구를 보여주자</p>
-                    <a href="#" class="stretched-link">자세히보기</a>
-                </div>
-
-            </div>
-        </div>
-        <div class="col-md-10 text-start">
-            <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-                <div class="col p-4 d-flex flex-column position-static">
-                    <strong class="d-inline-block mb-2 text-primary">이거는 태그?</strong>
-                    <h3 class="mb-0">게시물 제목!</h3>
-                    <div class="mb-1 text-body-secondary">게시글 작성 일자 ( 05.11.2023 )</div>
-                    <p class="card-text mb-auto">게시글 내용 미리보기 문구를 보여주자</p>
-                    <a href="#" class="stretched-link">자세히보기</a>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-10 text-start">
-            <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-                <div class="col p-4 d-flex flex-column position-static">
-                    <strong class="d-inline-block mb-2 text-primary">이거는 태그?</strong>
-                    <h3 class="mb-0">게시물 제목!</h3>
-                    <div class="mb-1 text-body-secondary">게시글 작성 일자 ( 05.11.2023 )</div>
-                    <p class="card-text mb-auto">게시글 내용 미리보기 문구를 보여주자</p>
-                    <a href="#" class="stretched-link">자세히보기</a>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-10 text-start">
-            <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-                <div class="col p-4 d-flex flex-column position-static">
-                    <strong class="d-inline-block mb-2 text-primary">이거는 태그?</strong>
-                    <h3 class="mb-0">게시물 제목!</h3>
-                    <div class="mb-1 text-body-secondary">게시글 작성 일자 ( 05.11.2023 )</div>
-                    <p class="card-text mb-auto">게시글 내용 미리보기 문구를 보여주자</p>
-                    <a href="#" class="stretched-link">자세히보기</a>
+                    <strong class="d-inline-block mb-2 text-primary">{{ board.category }}</strong>
+                    <h3 class="mb-0">{{ board.title }}</h3>
+                    <div class="mb-1 text-body-secondary">{{ board.createdAt }}</div>
+                    <p class="card-text mb-auto">{{ board.nickName }}</p>
+                    <router-link :to="`/boards/${board.boardId}`" @click="fetchItem(board.boardId)">자세히보기</router-link>
                 </div>
             </div>
         </div>
@@ -51,10 +17,40 @@
 </template>
 
 <script lang="ts">
-import { Vue } from 'vue-class-component';
+import BoardType, { BoardData, BoardPageType } from '@/types/BoardType';
+import { PropType, ref } from 'vue';
+import { getBoards } from '@/api/AxiosService';
+import { useStore } from 'vuex';
 
-export default class BoardList extends Vue {
+export default {
+    name : 'BoardList',
+    props: {
+    boards: {
+        required: true,
+        type: Array as PropType<BoardData[]> ,
+        // type: Array as PropType<BoardType[]>
+    }
+  }, 
+  setup() {
+    const store = useStore();
+    const selectedItem = ref<BoardType | null>(null);
 
+    const fetchItem = async (id: number) => {
+      try {
+        const response = await getBoards(id);
+        selectedItem.value = response.data.data;
+        console.log("click detail");
+        store.state.boards.boards = response.data.data;
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    return {
+        selectedItem,
+        fetchItem
+    }
+  }
 }
 </script>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,36 +1,57 @@
 <template>
-  <div class="row g-5">
-    <board-list></board-list>
+  <div class="row g-1">
+    <board-list :boards="boards"></board-list>
     <side-var></side-var>
-    <!-- TO-DO 페이지네이션으로 페이지 수 받아오면 v-for로 리펙토링 -->
-    <div class="page-conatiner">
-        <a href="#" class="stretched-link">1</a>  
-        <sapn>  2   </sapn>
-        <sapn>  3   </sapn>
-        <sapn>  4   </sapn>
-    </div>
+    <!-- TO-DO 다음 페이지 데이터 입히기-->
+    <nav aria-label="Page navigation example">
+      <ul class="pagination justify-content-center">
+        <li class="page-item" v-for="idx in totalPages" :key="idx"><a class="page-link" href="#">{{ idx }}</a></li>
+      </ul>
+    </nav>
   </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component'
 import BoardList from '@/components/BoardList.vue'
 import SideVar from '@/components/SideVar.vue'
+import { onMounted, PropType, reactive, ref, toRefs } from 'vue'
+import { useStore } from 'vuex'
+import { defineComponent, computed } from 'vue';
 
-@Options({
+export default {
+  name: 'HomeView',
   components: {
     BoardList,
     SideVar
-  }
-})
-export default class HomeView extends Vue {
+  },
+  setup() {
+    const store = useStore(); // store 객체 사용을 위해 선언
+    const boards = computed(() => store.state.boards.contents); // 미리 값 세팅
+    const totalPages = computed(() => store.state.boards.totalPages);
 
+    // 게시글 리스트 조회 함수 정의
+    const fetchData = (page: number) => {
+      store.dispatch('fetchBoards', page);
+    };
+    // 페이지 렌더링 시, 실행되는 함수
+    onMounted(() => {
+      const id = 0;
+      fetchData(id);
+    });
+
+    return {
+      boards,
+      totalPages,
+      fetchData,
+    };
+
+  },
 }
+
 </script>
 
 <style scoped>
-.page-conatiner {
-    margin-bottom: 20px;
+#page-conatiner {
+  margin-bottom: 20px;
 }
-
 </style>
