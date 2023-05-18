f4871763b110171fb3fc6e053d6aad22c36326e1
dldmsql <yhy5913@gmail.com>
2023-05-18 09:59:21 +0900

    style: CSS 변경

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,9 +8,9 @@
             </a>
         </div>
         <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-            <li><router-link to="/intro">오도리</router-link></li>
-            <li><router-link to="/skill">기술적용</router-link></li>
-            <li><router-link to="/review">회고</router-link></li>
+            <li><router-link style=text-decoration:none;color:black; to="/intro">오도리</router-link></li>
+            <li><router-link style=text-decoration:none;color:black; to="/skill">기술적용</router-link></li>
+            <li><router-link style=text-decoration:none;color:black; to="/review">회고</router-link></li>
         </ul>
 
         <div class="col-md-3 text-end" id="btn-container">
@@ -31,7 +31,8 @@ export default class BaseHeader extends Vue {
 <style scoped>
 
 li {
-    padding: 10px;
+    padding-left: 30px;
+    padding-right: 30px;
 }
 #btn-container {
     margin-right:110px;
