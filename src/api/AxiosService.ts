4ac0d267852da73038416590d7077348c7c7dcb5
dldmsql <yhy5913@gmail.com>
2023-05-18 16:45:10 +0900

    feat: 카테고리에 따른 게시글 리스트 조회 api 정의

--- src/api/AxiosService.ts
+++ src/api/AxiosService.ts
@@ -8,11 +8,16 @@ const restApi = axios.create({
 })
 
 // 게시글 단일 조회
-export function getBoards(id: number) : AxiosPromise<BoardResponse> {
+export function getBoard(id: number) : AxiosPromise<BoardResponse> {
     return restApi.get(`/api/v1/boards/${id}`);
 }
 
 // 게시글 리스트 조회
-export function fetchBoards(page : number) : AxiosPromise<BoardPageResponse> {
+export function getBoards(page : number) : AxiosPromise<BoardPageResponse> {
     return restApi.get(`/api/v1/boards?category=INTRO&size=4&page=${page}`);
+}
+
+// 말머리에 따른 리스트 조회
+export function getBoardsWithCategory(name : string) : AxiosPromise<BoardPageResponse> {
+    return restApi.get(`/api/v1/boards/board-category/search?name=${name}&keyword=`);
 }
