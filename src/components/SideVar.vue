2f2fe9e81c05cc80c92d82d8d179f0821544349f
dldmsql <yhy5913@gmail.com>
2023-05-25 19:58:59 +0900

    feat: 카테고리에 따른 필터링 적용

--- src/components/SideVar.vue
+++ src/components/SideVar.vue
@@ -5,9 +5,9 @@
             <div class="p-4">
                 <h4 class="fst-italic">Category</h4>
                 <ol class="list-unstyled mb-0">
-                    <li><router-link :to="`/`" @click="getHotRecordList(parts[0])">{{ parts[0] }}</router-link></li>
-                    <li><router-link :to="`/`" @click="getHotRecordList(parts[1])">{{ parts[1] }}</router-link></li>
-                    <li><router-link :to="`/`" @click="getHotRecordList(parts[2])">{{ parts[2] }}</router-link></li>
+                    <li><router-link :to="`/`" @click="getRecordList(parts[0])">{{ parts[0] }}</router-link></li>
+                    <li><router-link :to="`/`" @click="getRecordList(parts[1])">{{ parts[1] }}</router-link></li>
+                    <li><router-link :to="`/`" @click="getRecordList(parts[2])">{{ parts[2] }}</router-link></li>
                 </ol>
             </div>
             <!-- 금주의 인기 게시물 리스트 ( TOP 3 ) -->
@@ -25,7 +25,7 @@
 import { Vue } from 'vue-class-component';
 import { useStore } from 'vuex';
 import { computed, onMounted, PropType, ref } from 'vue';
-import { RecordData, RecordHotType } from '@/types/RecordType';
+import { RecordData, RecordHotType, RecordReqParam } from '@/types/RecordType';
 
 export default {
     name: 'SideVar',
@@ -37,7 +37,18 @@ export default {
     },
     setup() {
     const store = useStore();
-    const parts = ['WEB', 'SERVER', 'INFRA'];
+    const parts = ['web', 'server', 'infra'];
+
+    const getRecordList = (part: string) => {
+        console.log(part);
+        const reqParam : RecordReqParam = { // To-Do 동적으로 요청 param 바뀌도록
+        page: 0,
+        part : part,
+        type: 'tech',
+        category: 'disaster' // 이거 어떻게 처리할지 고민
+      };
+        store.dispatch('getRecordList', reqParam);
+    }
 
     const getRecord = (id: number) => {
         store.dispatch('getRecord', id);
@@ -46,6 +57,7 @@ export default {
 
     return {
         getRecord,
+        getRecordList,
         parts,
     }
   }
