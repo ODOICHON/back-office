682df8e3477209d45fe96976c9e0af30440bbba7
dldmsql <yhy5913@gmail.com>
2023-05-26 16:02:50 +0900

    feat: 마크다운 형식의 데이터 Viewer 구현

--- src/components/RecordDetail.vue
+++ src/components/RecordDetail.vue
@@ -1,59 +1,59 @@
 <template>
     <article class="blog-post">
-        <strong class="d-inline-block mb-2 text-primary">{{ selectedItem?.part }}</strong>
+        <p class="blog-post-meta">{{ selectedItem?.create_at }} by {{ selectedItem?.nick_name }}   <strong class="d-inline-block mb-2 text-primary"> {{ selectedItem?.part }}</strong></p> 
         <h2 class="blog-post-title mb-1">{{ selectedItem?.title }}</h2>
-        <p class="blog-post-meta">{{ selectedItem?.create_at }} by {{ selectedItem?.nick_name }}</p>
-
         
-        <p class="blog-post-content"> {{ selectedItem?.content }}</p>
-        <!-- <div id="viewer" class="markdown-content" v-if="selectedItem?.content != null"></div> -->
-
+        <div id="viewer" class="overflow-hidden flex-md-row mb-4 h-md-250 position-relative"> </div>
     </article>
 </template>
-  
+
 <script lang="ts">
-import { defineComponent, ref, onMounted, computed } from 'vue';
+import { defineComponent, onMounted, computed } from 'vue';
 import { useRoute } from 'vue-router';
 import { useStore } from 'vuex';
 import '@toast-ui/editor/dist/toastui-editor-viewer.css';
 
-import { Viewer } from '@toast-ui/editor'; // 수정된 import 문
-import Editor from '@toast-ui/editor';
+import { Editor, EditorCore, Viewer } from '@toast-ui/editor'; // 수정된 import 문
 
 export default defineComponent({
     name: 'RecordDetail',
+    components : {
+        viewer: document.querySelector('#viewer') as HTMLElement,
+    },
     setup() {
         const route = useRoute();
         const store = useStore();
         const recordId = computed(() => route.params.id);
         const selectedItem = computed(() => store.state.records.records);
 
-        // const viewer = ref<Viewer>(); // Viewer 타입으로 변경
-
         onMounted(() => {
-            // const viewerElement = document.querySelector('#viewer') as HTMLElement;
-            // viewer.value = Editor.factory({ // Viewer.factory()로 변경
-            //     el: viewerElement,
-            //     viewer: true,
-            //     height: '500px',
-            //     initialValue: '# hello'
-            // });
-
             getRecord(Number(recordId.value));
         });
         const getRecord = (id: number) => {
             store.dispatch('getRecord', id).then(() => {
-                // if (viewer.value) {
-                //     viewer.value.setMarkdown(selectedItem.value?.content);
-                // }
+                const v = EditorCore.factory({
+                    el: document.querySelector('#viewer') as HTMLElement,
+                    initialValue : selectedItem.value.content,
+                    viewer: true,
+                }) as Viewer
             });
         };
 
         return {
             selectedItem,
-            // viewer
         };
     },
+    
 });
 </script>
+  
+<style scoped>
+article {
+    padding-left: 70px;
+    padding-right: 70px;
+    margin-bottom: 40px;
+    margin-top: 40px;
+    height: 500px;
+}
+</style>
   
