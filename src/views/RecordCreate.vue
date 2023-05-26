e207ad79e71dfb1d6d3f8be7247273ff209710f9
dldmsql <yhy5913@gmail.com>
2023-05-26 16:02:32 +0900

    fix: 게시글 마크다운 형식으로 데이터 추출

--- src/views/RecordCreate.vue
+++ src/views/RecordCreate.vue
@@ -1,88 +1,3 @@
-<script setup lang="ts">
-import router from '@/router';
-import { authComputed } from '@/store/helper';
-import { RecordCreateReq } from '@/types/RecordType';
-import Editor from '@toast-ui/editor';
-import '@toast-ui/editor/dist/toastui-editor.css';
-import { defineComponent, onMounted, ref } from 'vue';
-import { useStore } from 'vuex';
-
-const props = defineProps({
-  modelValue: {
-    type: String,
-    required: false,
-    default: '',
-  },
-});
-const store = useStore(); // store 객체 사용을 위해 선언
-const emit = defineEmits(['update:modelValue']);
-const editor = ref();
-onMounted(() => {
-  const e = new Editor({
-    el: editor.value,
-    height: '500px',
-    initialEditType: 'markdown',
-    previewStyle: 'vertical',
-    events: {
-      change: () => emit('update:modelValue', e.getMarkdown()),
-    },
-
-  });
-});
-const title = ref('');
-const parts = [
-  { id: 'web', name: 'WEB' },
-  { id: 'server', name: 'SERVER' },
-  { id: 'infra', name: 'INFRA' },
-  { id: 'all', name: 'ALL' },
-];
-const selectedPart = ref(parts.find(part => part.name === 'WEB').id);
-const types = [
-  { id: 'odori', name: 'odori' },
-  { id: 'retro', name: 'retro' },
-  { id: 'tech', name: 'tech' },
-];
-const selectedType = ref(types.find(type => type.name === 'tech').id);
-
-const categories = [
-  { id: 'disaster', name: 'disaster' },
-  { id: 'issue', name: 'issue' },
-  { id: 'new_tech', name: 'new_tech' },
-  { id: 'architecture', name: 'architecture' },
-];
-const selectedCategory = ref(categories.find(category => category.name === 'disaster').id);
-
-const handleSubmit = (event) => {
-  event.preventDefault();
-  // Perform form submission logic here
-
-  const result = confirm('리뷰 신청하시겠습니까?');
-  if (result) {
-    console.log('editor', editor.value); // Check the editor ref
-    const contentElement = editor.value?.querySelector('.toastui-editor');
-    console.log('contentElement', contentElement); // Check the content element
-    const markdownContent = contentElement?.innerHTML;
-    console.log('markdownContent', markdownContent); // Check the markdown content
-    const createRecord: RecordCreateReq = {
-      title: title.value,
-      content: markdownContent,
-      part: selectedPart.value,
-      category: selectedCategory.value,
-      type: selectedType.value,
-    }
-    store.dispatch('createRecord', createRecord).then(
-      () => {
-        const recordId = store.state.records.recordId;
-        router.push({ name: 'RecordDetail', params: { id: recordId } });
-      }
-    );
-  } else {
-    alert('리뷰 신청을 취소하셨습니다.');
-  }
-};
-
-</script>
-
 <template>
   <form @submit.prevent="handleSubmit">
     <div class="row">
@@ -98,7 +13,7 @@ const handleSubmit = (event) => {
       <div class="col">
         <label for="type" class="form-label">TYPE 선택:</label>
         <select id="type" class="form-select" v-model="selectedType">
-          <option v-for="type in types" :key="type.id" :value="type.name">{{ type.name }}</option>
+          <option v-for="t in types" :key="t.id" :value="t.name">{{ t.name }}</option>
         </select>
       </div>
       <div class="col" v-if="selectedType === 'tech'">
@@ -108,12 +23,110 @@ const handleSubmit = (event) => {
         </select>
       </div>
     </div>
-    <div ref="editor" class="row"></div>
+    <div id="editor" ref="editor" class="row"></div>
     <div class="col text-end" id="btn-container">
       <button type="submit" class="btn btn-outline-primary me-2">리뷰 신청</button>
     </div>
   </form>
 </template>
+
+<script lang="ts">
+import router from '@/router';
+import { authComputed } from '@/store/helper';
+import { RecordCreateReq } from '@/types/RecordType';
+import Editor from '@toast-ui/editor';
+import '@toast-ui/editor/dist/toastui-editor.css';
+import { defineComponent, onMounted, ref, defineEmits } from 'vue';
+import { useStore } from 'vuex';
+
+export default defineComponent({
+  name: 'RecordCreate',
+  props: {
+    modelValue: {
+      type: String,
+      required: false,
+      default: '',
+    },
+  },
+  setup(props, { emit }) {
+    const store = useStore();
+    const editor = ref<Editor>();
+
+    onMounted(() => {
+      editor.value = new Editor({
+        el: document.querySelector('#editor') as HTMLElement ,
+        height: '500px',
+        initialEditType: 'markdown',
+        previewStyle: 'vertical',
+        events: {
+          change: () => {
+            emit('update:modelValue', editor.value?.getMarkdown());
+          },
+        },
+      });
+    });
+
+    const title = ref('');
+    const parts = [
+      { id: 'web', name: 'WEB' },
+      { id: 'server', name: 'SERVER' },
+      { id: 'infra', name: 'INFRA' },
+      { id: 'all', name: 'ALL' },
+    ];
+    const selectedPart = ref(parts.find(part => part.name === 'WEB').id);
+    const types = [
+      { id: 'odori', name: 'odori' },
+      { id: 'retro', name: 'retro' },
+      { id: 'tech', name: 'tech' },
+    ];
+    const selectedType = ref(types.find(type => type.name === 'tech').id);
+
+    const categories = [
+      { id: 'disaster', name: 'disaster' },
+      { id: 'issue', name: 'issue' },
+      { id: 'new_tech', name: 'new_tech' },
+      { id: 'architecture', name: 'architecture' },
+    ];
+    const selectedCategory = ref(categories.find(category => category.name === 'disaster').id);
+
+    const handleSubmit = (event) => {
+      event.preventDefault();
+
+      const result = confirm('리뷰 신청하시겠습니까?');
+      if (result) {
+        const markdownContent = editor.value?.getMarkdown();
+        // console.log('markdownContent', markdownContent);
+
+        const createRecord: RecordCreateReq = {
+          title: title.value,
+          content: markdownContent!,
+          part: selectedPart.value,
+          category: selectedCategory.value,
+          type: selectedType.value,
+        };
+        store.dispatch('createRecord', createRecord).then(() => {
+          const recordId = store.state.records.recordId;
+          router.push({ name: 'RecordDetail', params: { id: recordId } });
+        });
+      } else {
+        alert('리뷰 신청을 취소하셨습니다.');
+      }
+    };
+
+    return {
+      title,
+      parts,
+      selectedPart,
+      types,
+      selectedType,
+      categories,
+      selectedCategory,
+      handleSubmit,
+    };
+  },
+});
+</script>
+
 <style scoped>
 form {
   padding-left: 70px;
