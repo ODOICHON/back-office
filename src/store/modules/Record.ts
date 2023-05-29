57bd3e64c98bc6eb5d6eeb219d4ddb740c1f0e0e
dldmsql <yhy5913@gmail.com>
2023-05-29 20:35:00 +0900

    feat: 마이페이지 게시글 데이터 조회 연동

--- src/store/modules/Record.ts
+++ src/store/modules/Record.ts
@@ -1,6 +1,7 @@
-import { getRecordList, getRecord, createRecord, getHotRecordList, updateRecord, deleteRecord } from '@/api/Record';
+import { getRecordList, getRecord, createRecord, getHotRecordList, updateRecord, deleteRecord, getRecordListMine, getRecordListApply, getRecordListReviewed, getRecordListWait } from '@/api/Record';
 import { Module } from 'vuex';
 import RecordType, { RecordCreateReq, RecordData, RecordHotType, RecordPageType, RecordReqParam, RecordUpdateReq } from '@/types/RecordType';
+import { filters } from '@/constants/myPageFilter';
 
 // board에 대한 store 관리
 export const recordModule: Module<any, any> = {
@@ -8,6 +9,8 @@ export const recordModule: Module<any, any> = {
     records: {} as RecordType, // 게시글 단일 조회 시 받아오는 데이터
     contents : [] as RecordData[], // 페이징 처리로 받아오는 데이터
     hotRecords: [] as RecordHotType[], // 금주의 핫 게시물 데이터
+    myRecords: [] as RecordData[], // 마이페이지에서 조회하는 데이터
+    myPageFilter: '', // 마이페이지에서 사용되는 필터 ( 필터에 따른 페이징 처리를 위해 사용 )
     totalPages : 0,
     page : 0,
     recordId: 0,
@@ -33,6 +36,12 @@ export const recordModule: Module<any, any> = {
     },
     setHotRecordList(state, hotRecords : RecordHotType[]) {
       state.hotRecords = hotRecords;
+    },
+    setMyRecordList(state, myRecords : RecordData[]) {
+      state.myRecords = myRecords;
+    },
+    setMypageFilter(state, filter : string) {
+      state.myPageFilter = filter;
     }
   },
   actions: { // 비동기 처리를 할 때 사용한다. 
@@ -66,7 +75,6 @@ export const recordModule: Module<any, any> = {
       } catch (error) {
         console.error(error);
       }
-     
 
     },
 
@@ -100,6 +108,62 @@ export const recordModule: Module<any, any> = {
         console.log(response.data);
         commit('setRecordId', 0);
         commit('setRecord', null);
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    async getRecordListMine({commit, rootState}, id : number) {
+      try {
+        const headers = {
+          Authorization: `${rootState.auth.access_token}`,
+        };
+        const response = await getRecordListMine(id, headers);
+        commit('setMyRecordList', response.data.data.records.content);
+        commit('setTotalPages', response.data.data.records.totalPages);
+        commit('setPage', response.data.data.records.totalElements);
+        commit('setMypageFilter', filters[0]);
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    async getRecordListReviewed({commit, rootState}, id : number) {
+      try {
+        const headers = {
+          Authorization: `${rootState.auth.access_token}`,
+        };
+        const response = await getRecordListReviewed(id, headers);
+        commit('setMyRecordList', response.data.data.records.content);
+        commit('setTotalPages', response.data.data.records.totalPages);
+        commit('setPage', response.data.data.records.totalElements);
+        commit('setMypageFilter', filters[1]);
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    async getRecordListWait({commit, rootState}, id : number) {
+      try {
+        const headers = {
+          Authorization: `${rootState.auth.access_token}`,
+        };
+        const response = await getRecordListWait(id, headers);
+        commit('setMyRecordList', response.data.data.records.content);
+        commit('setTotalPages', response.data.data.records.totalPages);
+        commit('setPage', response.data.data.records.totalElements);
+        commit('setMypageFilter', filters[2]);
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    async getRecordListApply({commit, rootState}, id : number) {
+      try {
+        const headers = {
+          Authorization: `${rootState.auth.access_token}`,
+        };
+        const response = await getRecordListApply(id, headers);
+        commit('setMyRecordList', response.data.data.records.content);
+        commit('setTotalPages', response.data.data.records.totalPages);
+        commit('setPage', response.data.data.records.totalElements);
+        commit('setMypageFilter', filters[3]);
       } catch (error) {
         console.error(error);
       }
